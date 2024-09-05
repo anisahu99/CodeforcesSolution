db47ea97b4a0e5adf8724dfa0abc72af7d38784f
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2024-09-05 14:25:40 +0530

    Add Intuition and Approach

--- A_Buttons.cpp
+++ A_Buttons.cpp
@@ -3,6 +3,20 @@ using namespace std;
 
 #define endl "\n"
 
+/* 
+    Intuition:
+                The optimal way is everyone try to maximize  press by C type button.
+    Approach:
+    the Annal will get first turn i.e
+    if c is odd number then Anna will get one extra chance to press
+    if a+1>b i.e Anna have more chance to press and Anna will never be last one who has no button to press
+    if a+1<=b i.e Katie have more chance to press and Katie will never be last one who has no button to press
+
+    if c is even number then Anna and Katie will get same number of chance to press
+    if a>b i.e Anna have more chance to press and Anna will never be last one who has no button to press
+    if a<=b i.e Katie have more chance to press and Katie will never be last one who has no button to press
+*/
+
 void solve(int a, int b, int c){
     if(c&1) a++;
     if(a>b){
